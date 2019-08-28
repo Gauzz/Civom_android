e7b4eb81d26a47e08b981fdeee511affbd2c028d
Gaurav Rozatkar <gauravrozatkar1@gmail.com>
2019-08-28 13:04:06 +0530

    Retrofit integrated

---
+++ app/src/main/java/com/lateralx/civom/Model/RetroPhoto.java
@@ -0,0 +1,61 @@
+package com.lateralx.civom.Model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class RetroPhoto {
+
+    @SerializedName("albumId")
+    private Integer albumId;
+    @SerializedName("id")
+    private Integer id;
+    @SerializedName("title")
+    private String title;
+    @SerializedName("url")
+    private String url;
+    @SerializedName("thumbnailUrl")
+    private String thumbnailUrl;
+
+    public RetroPhoto(Integer albumId, Integer id, String title, String url, String thumbnailUrl) {
+        this.albumId = albumId;
+        this.id = id;
+        this.title = title;
+        this.url = url;
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public Integer getAlbumId() {
+        return albumId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setAlbumId(Integer albumId) {
+        this.albumId = albumId;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+}
